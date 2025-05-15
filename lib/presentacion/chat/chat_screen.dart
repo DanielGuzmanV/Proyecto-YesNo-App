fde49d99a1e868268e977c693b1c82c4bcc68826
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-05-15 19:06:03 -0400

    Se agrego el chatView y el listView

--- lib/presentacion/chat/chat_screen.dart
+++ lib/presentacion/chat/chat_screen.dart
@@ -21,6 +21,35 @@ class ChatScreen extends StatelessWidget {
         title: const Text('Doctor Simi'),
         centerTitle: false,
       ),
+
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+
+          children: [
+            Expanded(child: ListView.builder(
+              itemCount: 50,
+              itemBuilder: (context, index) {
+                return Text('El indice es $index');
+              },
+            )),
+        
+            const Text('Enviar mensajes')
+          ],  
+
+        ),
+      ) 
     );
   }
 }
