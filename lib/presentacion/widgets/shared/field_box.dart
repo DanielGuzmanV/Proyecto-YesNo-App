6b385427b6869c0713befda27f15a9abbd3e5fff
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-05-15 21:01:25 -0400

    se mejoro la caja de texto del teclado

--- lib/presentacion/widgets/shared/field_box.dart
+++ lib/presentacion/widgets/shared/field_box.dart
@@ -6,7 +6,13 @@ class FieldBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    // Creamos un solo codigo para el enableBorder y focusedBorder:
+    // customTextController nos ayudara a limpiar la caja de texto al enviar un texto
+    final customTextController = TextEditingController();
+
+    // customFocusNode mantendra el activo el teclado sin que se cierre
+    final customFocusNode = FocusNode();
+
+    // Creamos un solo codigo para el enableBorder y focusedBorder que seria la decoracion del border:
     final customInputBorder = UnderlineInputBorder(
       borderSide: const BorderSide(color: Colors.transparent),
       borderRadius: BorderRadius.circular(35)
@@ -14,19 +20,34 @@ class FieldBox extends StatelessWidget {
 
     // Decoracion del de la caja de texto:
     final customInputDecoration = InputDecoration(
-        enabledBorder: customInputBorder,
-        focusedBorder: customInputBorder,
+      hintText: 'El mensaje termino con "?"',
 
-        filled: true,
-        suffixIcon: IconButton(
-          onPressed: (){}, 
-          icon: const Icon(Icons.send_outlined)
-        )
-      );
+      enabledBorder: customInputBorder,
+      focusedBorder: customInputBorder,
+
+      filled: true,
+      suffixIcon: IconButton(
+        onPressed: (){
+          customTextController.clear();
+        }, 
+        icon: const Icon(Icons.send_outlined)
+      )
+    );
 
     return TextFormField(
+      // Cuando presionemos fuera del teclado este se cerrara
+      onTapOutside: (event) {
+        customFocusNode.unfocus();
+      },
+
+      focusNode: customFocusNode,
+      controller: customTextController,
       decoration: customInputDecoration,
 
+      onFieldSubmitted: (value) {
+        customTextController.clear();
+        customFocusNode.requestFocus();
+      },
     );
   }
 }
