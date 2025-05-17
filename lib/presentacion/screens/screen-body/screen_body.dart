ef8196846268f29257596032d50209af3bca2ec2
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-05-16 23:17:53 -0400

    Se configuro para mostrar los mensajes del provider

--- lib/presentacion/screens/screen-body/screen_body.dart
+++ lib/presentacion/screens/screen-body/screen_body.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yesno_app/domain/entities/message.dart';
+import 'package:yesno_app/presentacion/providers/chat_provider.dart';
 import 'package:yesno_app/presentacion/screens/screen-fieldbox/screen_fieldbox.dart';
 
 import 'package:yesno_app/presentacion/widgets/chat/doc_message.dart';
@@ -9,18 +12,26 @@ class ScreenBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Notificaremos los cambios:
+    final chatProviderChange = context.watch<ChatProvider>();
+
     return SafeArea(
         child: Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10),
       child: Column(
+
         children: [
           Expanded(
               child: ListView.builder(
-            itemCount: 30,
+            // Controlamos la dimension de los mensajes
+            itemCount: chatProviderChange.messageList.length,
             itemBuilder: (context, index) {
-              return (index % 2 == 0) 
-              ? const MyMessage() 
-              : const DocMessage();
+
+              final messageValues = chatProviderChange.messageList[index];
+
+              return (messageValues.fromWho == EnumFromWho.smsDoctor) 
+              ? const DocMessage()
+              : const MyMessage();
             },
           )),
 
