f80c77861df264d7fabce2233b9b4843232474e4
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-05-16 21:58:49 -0400

    se agrego el domain-entities

---
+++ lib/domain/entities/message.dart
@@ -0,0 +1,18 @@
+class ClassMessage {
+
+  final String textMessage;
+  final String? imageUrl;
+  final EnumFromWho fromWho;
+
+  ClassMessage({
+    required this.textMessage,
+    this.imageUrl, 
+    required this.fromWho});
+
+}
+
+enum EnumFromWho {
+  mine,
+  doctor
+}
+
